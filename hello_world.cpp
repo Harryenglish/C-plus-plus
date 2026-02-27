9be2b8623581886cd4a07bcfd620ded7269c2234
Harry English <s2488552@ed.ac.uk>
2026-02-27 22:12:07 +0000

    first programme

---
+++ hello_world.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int next() {
+    int trees = 10;
+    cout << "There are " << trees << " trees at the park \n";
+    return 0;
+}
+
+int main() {
+    cout << "Hello world! \n";
+    cout << "I am building my first C++ programme \n";
+
+    next();
+
+    return 0;
+}
